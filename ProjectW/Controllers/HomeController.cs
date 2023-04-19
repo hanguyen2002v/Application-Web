88716d4d0d59720025ce32ac3636db5f89151c3f
hanguyen2002v <hanvgch200689@fpt.edu.vn>
2023-04-19 17:35:28 +0700

    Detail and display book (home controller)

--- ProjectW/Controllers/HomeController.cs
+++ ProjectW/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using ProjectW.Models;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using ProjectW.Data;
-//using ProjectW.Models;
 
 namespace ProjectW.Controllers
 {
@@ -23,11 +22,35 @@ namespace ProjectW.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy(string searchString)
         {
-            var books = _context.Book.ToList();
-            return View(books);
+            if (_context.Book == null)
+            {
+                return Problem("Not book");
+            }
+
+            var books = from b in _context.Book select b;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await books.ToListAsync());
         }
+
+        //Trang chi tiết sản phẩm
+        public async Task<IActionResult> Detail(int id)
+        {
+            var book = await _context.Book.Include(b => b.Category)
+                            .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
